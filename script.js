36e7f60f53d1f3ce460d3641f9c1da1c7a1d9190
mlionhart <lionrhart@gmail.com>
2024-03-29 09:47:20 -0600

    added opacity to make full rgba color instead of just rgb

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 const range1 = document.getElementById("range");
 const range2 = document.getElementById("range2");
 const range3 = document.getElementById("range3");
+const range4 = document.getElementById("range4");
 const button = document.getElementById("get-color");
 const colorBox = document.getElementById("color-box");
 
 const redIndicator = document.getElementsByClassName("red-range-indicator");
 const greenIndicator = document.getElementsByClassName("green-range-indicator");
 const blueIndicator = document.getElementsByClassName("blue-range-indicator");
+const opacityIndicator = document.getElementsByClassName("opacity-indicator");
 
 window.onload = function () {
   range1.value = 0;
   range2.value = 0;
   range3.value = 0;
+  range4.value = 1;
   redIndicator[0].textContent = 0;
   greenIndicator[0].textContent = 0;
   blueIndicator[0].textContent = 0;
+  opacityIndicator[0].textContent = 1;
   var color =
-    "rgb(" + range1.value + "," + range2.value + "," + range3.value + ")";
+    "rgba(" + range1.value + ", " + range2.value + ", " + range3.value + ", " + range4.value + ")";
   colorValueField.value = color;
   colorBox.style.backgroundColor = color;
 };
@@ -27,16 +31,26 @@ colorValueBtn = document.getElementById("color-value-btn");
 redIndicator[0].textContent = range1.value;
 greenIndicator[0].textContent = range2.value;
 blueIndicator[0].textContent = range3.value;
+opacityIndicator[0].textContent = range4.value;
 
 displayRangeInput(redIndicator, range1);
 displayRangeInput(greenIndicator, range2);
 displayRangeInput(blueIndicator, range3);
+displayRangeInput(opacityIndicator, range4);
 
 function displayRangeInput(indicator, range) {
   range.addEventListener("input", function () {
     indicator[0].textContent = range.value;
     var color =
-      "rgb(" + range1.value + "," + range2.value + "," + range3.value + ")";
+      "rgba(" +
+      range1.value +
+      ", " +
+      range2.value +
+      ", " +
+      range3.value +
+      ", " +
+      range4.value +
+      ")";
     colorValueField.value = color;
     colorBox.style.backgroundColor = color;
   });
